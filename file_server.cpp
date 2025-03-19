a4457e41327167121fca2a5434f80656b25636ba
Andrew Shen-Costello <ashenco1@cs.binghamton.edu>
2025-03-19 18:09:07 -0400

    oops

--- file_server.cpp
+++ file_server.cpp
@@ -1,96 +1,112 @@
-#include<iostream>
-#include<fstream>
-#include<stdio.h>
-#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <stdlib.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <fstream>
 using namespace std;
-
-class Server_socket{
-    fstream file;
-
-    int PORT;
-    
-    int general_socket_descriptor;
-    int new_socket_descriptor;
-
-    struct sockaddr_in address;
-    int address_length;
-
-    public:
-        Server_socket(){
-            create_socket();
-            PORT = 8050;
-
-            address.sin_family = AF_INET; 
-            address.sin_addr.s_addr = INADDR_ANY; 
-            address.sin_port = htons( PORT );
-            address_length = sizeof(address);
-
-            bind_socket();
-            set_listen_set();
-            accept_connection();
-
-            file.open(".//Data//Server//server_text.txt", ios::in | ios::binary);
-            if(file.is_open()){
-                cout<<"[LOG] : File is ready to Transmit.\n";
-            }
-            else{
-                cout<<"[ERROR] : File loading failed, Exititng.\n";
-                exit(EXIT_FAILURE);
-            }
+//Server side
+int main(int argc, char *argv[])
+{
+    //for the server, we only need to specify a port number
+    if(argc != 2)
+    {
+        cerr << "Usage: port" << endl;
+        exit(0);
+    }
+    //grab the port number
+    int port = atoi(argv[1]);
+    //buffer to send and receive messages with
+    char msg[1500];
+     
+    //setup a socket and connection tools
+    sockaddr_in servAddr;
+    bzero((char*)&servAddr, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(port);
+ 
+    //open stream oriented socket with internet address
+    //also keep track of the socket descriptor
+    int serverSd = socket(AF_INET, SOCK_STREAM, 0);
+    if(serverSd < 0)
+    {
+        cerr << "Error establishing the server socket" << endl;
+        exit(0);
+    }
+    //bind the socket to its local address
+    int bindStatus = bind(serverSd, (struct sockaddr*) &servAddr, 
+        sizeof(servAddr));
+    if(bindStatus < 0)
+    {
+        cerr << "Error binding socket to local address" << endl;
+        exit(0);
+    }
+    cout << "Waiting for a client to connect..." << endl;
+    //listen for up to 5 requests at a time
+    listen(serverSd, 5);
+    //receive a request from client using accept
+    //we need a new address to connect with the client
+    sockaddr_in newSockAddr;
+    socklen_t newSockAddrSize = sizeof(newSockAddr);
+    //accept, create a new socket descriptor to 
+    //handle the new connection with client
+    int newSd = accept(serverSd, (sockaddr *)&newSockAddr, &newSockAddrSize);
+    if(newSd < 0)
+    {
+        cerr << "Error accepting request from client!" << endl;
+        exit(1);
+    }
+    cout << "Connected with client!" << endl;
+    //lets keep track of the session time
+    struct timeval start1, end1;
+    gettimeofday(&start1, NULL);
+    //also keep track of the amount of data sent as well
+    int bytesRead, bytesWritten = 0;
+    while(1)
+    {
+        //receive a message from the client (listen)
+        cout << "Awaiting client response..." << endl;
+        memset(&msg, 0, sizeof(msg));//clear the buffer
+        bytesRead += recv(newSd, (char*)&msg, sizeof(msg), 0);
+        if(!strcmp(msg, "exit"))
+        {
+            cout << "Client has quit the session" << endl;
+            break;
         }
-
-        void create_socket(){
-            if ((general_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
-                perror("[ERROR] : Socket failed");
-                exit(EXIT_FAILURE);
-            }
-            cout<<"[LOG] : Socket Created Successfully.\n";
+        cout << "Client: " << msg << endl;
+        cout << ">";
+        string data;
+        getline(cin, data);
+        memset(&msg, 0, sizeof(msg)); //clear the buffer
+        strcpy(msg, data.c_str());
+        if(data == "exit")
+        {
+            //send to the client that server has closed the connection
+            send(newSd, (char*)&msg, strlen(msg), 0);
+            break;
         }
-
-        void bind_socket(){
-            if (bind(general_socket_descriptor, (struct sockaddr *)&address, sizeof(address))<0) {
-                perror("[ERROR] : Bind failed");
-                exit(EXIT_FAILURE);
-            }
-            cout<<"[LOG] : Bind Successful.\n";
-        }
-
-        void set_listen_set(){
-            if (listen(general_socket_descriptor, 3) < 0) {
-                perror("[ERROR] : Listen");
-                exit(EXIT_FAILURE);
-            }
-            cout<<"[LOG] : Socket in Listen State (Max Connection Queue: 3)\n";
-        }
-
-        void accept_connection(){
-            if ((new_socket_descriptor = accept(general_socket_descriptor, (struct sockaddr *)&address, (socklen_t*)&address_length))<0) { 
-                perror("[ERROR] : Accept");
-                exit(EXIT_FAILURE);
-            }
-            cout<<"[LOG] : Connected to Client.\n";
-        }
-
-        void transmit_file(){
-            std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-            cout<<"[LOG] : Transmission Data Size "<<contents.length()<<" Bytes.\n";
-
-            cout<<"[LOG] : Sending...\n";
-
-            int bytes_sent = send(new_socket_descriptor , contents.c_str() , contents.length() , 0 );
-            cout<<"[LOG] : Transmitted Data Size "<<bytes_sent<<" Bytes.\n";
-
-            cout<<"[LOG] : File Transfer Complete.\n";
-        }
-};
-
-int main(){
-    Server_socket S;
-    S.transmit_file();
-    return 0;
+        //send the message to client
+        bytesWritten += send(newSd, (char*)&msg, strlen(msg), 0);
+    }
+    //we need to close the socket descriptors after we're all done
+    gettimeofday(&end1, NULL);
+    close(newSd);
+    close(serverSd);
+    cout << "********Session********" << endl;
+    cout << "Bytes written: " << bytesWritten << " Bytes read: " << bytesRead << endl;
+    cout << "Elapsed time: " << (end1.tv_sec - start1.tv_sec) 
+        << " secs" << endl;
+    cout << "Connection closed..." << endl;
+    return 0;   
 }
