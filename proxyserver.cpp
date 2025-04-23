8e307d787cfc4ff52bc0436b74cea8f3882d7284
Andrew Shen-Costello <ashenco1@cs.binghamton.edu>
2025-04-23 17:10:33 -0400

    first working example

---
+++ proxyserver.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <fstream>
+#include <thread>
+#include <vector>
+#include <signal.h>
+#include <unordered_map>
+#include <atomic>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+
+#define REQUEST_BUFFER_LENGTH 1500
+#define FILE_BUFFER_LENGTH 1024
+atomic<bool> termFlag{false};
+int proxySd = socket(AF_INET, SOCK_STREAM, 0);
+
+void forward_response(int newClientSd, int newServerSd)
+{
+    char buf[1500] = {0};
+    int bytesRead = recv(newServerSd, (char*)buf, 1500, 0);
+    while (bytesRead > 0)
+    {
+        send(newClientSd, buf, bytesRead, 0);
+        bytesRead = recv(newServerSd, (char*)buf, 1500, 0);
+    }
+}
+
+void worker(int newClientSd, int newServerSd) 
+{  
+    cout << "new thread!" << endl;
+    time_t time;
+    char msg[REQUEST_BUFFER_LENGTH] = {0};
+    int bytesRead = recv(newClientSd, (char*)msg, REQUEST_BUFFER_LENGTH, 0);
+    if(!strcmp(msg, "exit"))
+    {
+        cout << "Client has quit the session" << endl;
+        return;
+    }
+    send(newServerSd, msg, bytesRead, 0);
+    time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    cout << "proxy-forward," << "server" << "," << this_thread::get_id() << "," << ctime(&time) << endl;
+    forward_response(newClientSd, newServerSd);
+    time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    cout << "proxy-forward," << "client" << "," << this_thread::get_id() << "," << ctime(&time) << endl;
+    close(newClientSd);
+    close(newServerSd);
+    return;
+        
+}
+
+void int_handler(int n)
+{
+    cout << "Waiting for active threads" << endl;
+    termFlag.store(true);
+    close(proxySd);
+    cout << "Bye" << endl;
+    // exit(0);
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    // ignore sigpipe so we don't terminate whenever a client disconnects unexpectedly
+    signal(SIGPIPE, SIG_IGN);
+    //need to catch ctrl+c so we can kill all threads (SIGTERM)
+    signal(SIGINT, int_handler);
+    //for the server, we only need to specify a port number
+    int port;
+    if(argc != 4)
+    {
+        cerr << "Usage: <proxy_port> <server_ip> <server_port>" << endl;
+        exit(0);
+    }
+    //grab the port number
+    port = atoi(argv[1]);
+     
+    //setup a socket and connection tools
+    sockaddr_in proxyAddr;
+    bzero((char*)&proxyAddr, sizeof(proxyAddr));
+    proxyAddr.sin_family = AF_INET;
+    proxyAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    proxyAddr.sin_port = htons(port);
+
+    char *serverIp = argv[2]; port = atoi(argv[3]); 
+    struct hostent* host = gethostbyname(serverIp); 
+    sockaddr_in serverSockAddr;   
+    bzero((char*)&serverSockAddr, sizeof(serverSockAddr)); 
+    serverSockAddr.sin_family = AF_INET; 
+    serverSockAddr.sin_addr.s_addr = 
+        inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list));
+    serverSockAddr.sin_port = htons(port);
+    
+    //open stream oriented socket with internet address
+    //also keep track of the socket descriptor
+    
+    if(proxySd < 0)
+    {
+        cerr << "Error establishing the server socket" << endl;
+        exit(0);
+    }
+    //bind the socket to its local address
+    int bindStatus = bind(proxySd, (struct sockaddr*) &proxyAddr, sizeof(proxyAddr));
+    if(bindStatus < 0)
+    {
+        cerr << "Error binding socket to local address" << endl;
+        exit(0);
+    }
+    vector<thread> threadList;
+    cout << "Waiting for a client to connect..." << endl;
+
+    while (!termFlag.load())
+    {
+        listen(proxySd, 5);
+        //receive a request from client using accept
+        //we need a new address to connect with the client
+        sockaddr_in newClientSockAddr;
+        socklen_t newClientSockAddrSize = sizeof(newClientSockAddr);
+        
+        int newClientSd = accept(proxySd, (sockaddr *)&newClientSockAddr, &newClientSockAddrSize);
+        if(newClientSd < 0)
+        {
+            if(termFlag.load())
+                break;
+            cerr << "Error accepting request from client!" << endl;
+            continue;
+        }
+
+        int newServerSd = socket(AF_INET, SOCK_STREAM, 0);
+        int status = connect(newServerSd, (sockaddr*) &serverSockAddr, sizeof(serverSockAddr));
+        if(status < 0)
+        {
+            cout << "Error connecting to webserver!" << endl;
+            continue;
+        }
+        threadList.push_back(thread(worker, newClientSd, newServerSd));
+    }
+    for (auto &t : threadList) {
+        if (t.joinable()){
+            t.join();
+        }
+    }
+
+    return 0;   
+}
--- webserver.cpp
+++ webserver.cpp
@@ -14,23 +14,35 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <fstream>
-#include <map>
+#include <thread>
+#include <vector>
+#include <signal.h>
+#include <unordered_map>
+#include <atomic>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 
 #define REQUEST_BUFFER_LENGTH 1500
 #define FILE_BUFFER_LENGTH 1024
+atomic<bool> termFlag{false};
+int serverSd = socket(AF_INET, SOCK_STREAM, 0);
 
-string matchMimeType(string path) {
-    if (path.size() >= 4) {
+
+string matchMimeType(string path) 
+{
+    if (path.size() >= 4) 
+    {
         int dotPlace = path.find_last_of(".");
-        map<string, string> contentTypes = {
+        unordered_map<string, string> contentTypes = 
+        {
             {".html", "text/html"},
             {".pdf", "application/pdf"},
             {".css","text/css"},            
             {".jpeg","image/jpeg"},            
             {".jpg","image/jpeg"},   
-            {".ico","image/x-icon"},                          
+            {".ico","image/x-icon"},             
         };
         return contentTypes[path.substr(dotPlace)];
     }
@@ -58,18 +70,27 @@ int process_get_request(int socket, char request_buffer[REQUEST_BUFFER_LENGTH], 
         else
         {
             cout<<"Error loading file " << path << ", Exititng.\n";
-            string not_found = "HTTP/1.1 404 File not found\r\n";
+            string not_found = "HTTP/1.1 404 File not found\r\n"
+                "Content-Type: text/html\r\n"
+                "Content-Length: 123\r\n"
+                "\r\n"
+                "<!DOCTYPE html>\r\n"
+                "<html>\r\n"
+                "<head><title>404 Not Found</title></head>\r\n"
+                "<body>\r\n"
+                "<h1>404 File Not Found</h1>\r\n"
+                "</body>\r\n"
+                "</html>\r\n";
             send(socket, not_found.c_str(), not_found.size(), 0);
-            return 1;
+            return 404;
         }
         int bytes_read = 0;
-        
         string contentType = matchMimeType(path);
         cout << "contentType is: " << contentType << endl;
-        
         string response = "HTTP/1.1 200 OK\r\n"
             "Content-Type: " + contentType + "\r\n" 
             "Content-Length: " + to_string(file_size) + "\r\n"
+            "Connection: close\r\n"
             "\r\n";
         send(socket, response.c_str(), response.size(), 0);
         char file_buffer[FILE_BUFFER_LENGTH];
@@ -82,31 +103,71 @@ int process_get_request(int socket, char request_buffer[REQUEST_BUFFER_LENGTH], 
             if (sent == -1) break;
         }
         file.close();
+        return 200;
     } 
     else 
     {
-        string not_found = "HTTP/1.1 400 Bad request\r\n";
+        string not_found = "HTTP/1.1 400 Bad request\r\n"
+            "Content-Type: text/html\r\n"
+            "Content-Length: 100\r\n"
+            "\r\n"
+            "<!DOCTYPE html>\r\n"
+            "<html>\r\n"
+            "<head><title>400 Bad Request</title></head>\r\n"
+            "<body>\r\n"
+            "<h1>400 Bad Request</h1>\r\n"
+            "</body>\r\n"
+            "</html>\r\n";
         send(socket, not_found.c_str(), not_found.size(), 0);
-        return 1;
+        return 400;
     }
-    return 0;
 }
 
-//Server side
+
+
+void worker(int newSd) 
+{  
+    cout << "new thread!" << endl;
+    char msg[REQUEST_BUFFER_LENGTH] = {0};
+    int bytesRead = recv(newSd, (char*)msg, REQUEST_BUFFER_LENGTH, 0);
+    if(!strcmp(msg, "exit"))
+    {
+        cout << "Client has quit the session" << endl;
+        return;
+    }
+    int response_code = process_get_request(newSd, msg, bytesRead);
+    close(newSd);
+    time_t time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    cout << "server-response," << response_code << "," << this_thread::get_id() << "," << ctime(&time) << endl;
+    return;
+        
+}
+
+void int_handler(int n)
+{
+    cout << "Waiting for active threads" << endl;
+    termFlag.store(true);
+    close(serverSd);
+    cout << "Bye" << endl;
+    // exit(0);
+    return;
+}
+
 int main(int argc, char *argv[])
 {
     // ignore sigpipe so we don't terminate whenever a client disconnects unexpectedly
     signal(SIGPIPE, SIG_IGN);
+    //need to catch ctrl+c so we can kill all threads (SIGTERM)
+    signal(SIGINT, int_handler);
     //for the server, we only need to specify a port number
+    int port;
     if(argc != 2)
     {
         cerr << "Usage: port" << endl;
         exit(0);
     }
     //grab the port number
-    int port = atoi(argv[1]);
-    //buffer to send and receive messages with
-    char msg[REQUEST_BUFFER_LENGTH];
+    port = atoi(argv[1]);
      
     //setup a socket and connection tools
     sockaddr_in servAddr;
@@ -117,7 +178,7 @@ int main(int argc, char *argv[])
  
     //open stream oriented socket with internet address
     //also keep track of the socket descriptor
-    int serverSd = socket(AF_INET, SOCK_STREAM, 0);
+    
     if(serverSd < 0)
     {
         cerr << "Error establishing the server socket" << endl;
@@ -130,39 +191,35 @@ int main(int argc, char *argv[])
         cerr << "Error binding socket to local address" << endl;
         exit(0);
     }
+    vector<thread> threadList;
     cout << "Waiting for a client to connect..." << endl;
-    //listen for up to 5 requests at a time
-    listen(serverSd, 5);
-    //receive a request from client using accept
-    //we need a new address to connect with the client
-    sockaddr_in newSockAddr;
-    socklen_t newSockAddrSize = sizeof(newSockAddr);
-    
-    while(1)
+
+    while (!termFlag.load())
     {
-        //accept, create a new socket descriptor to 
-        //handle the new connection with client
+        listen(serverSd, 5);
+        //receive a request from client using accept
+        //we need a new address to connect with the client
+        sockaddr_in newSockAddr;
+        socklen_t newSockAddrSize = sizeof(newSockAddr);
+        
         int newSd = accept(serverSd, (sockaddr *)&newSockAddr, &newSockAddrSize);
         if(newSd < 0)
         {
+            if(termFlag.load())
+                break;
             cerr << "Error accepting request from client!" << endl;
-            break;
+            continue;
         }
-        cout << "Connected with client!" << endl;
-        
-        memset(msg, 0, sizeof(msg));
-        int bytesRead = recv(newSd, (char*)&msg, sizeof(msg), 0);
-        cout << bytesRead << " " << sizeof(msg) << endl;
-        if(!strcmp(msg, "exit"))
-        {
-            cout << "Client has quit the session" << endl;
-            break;
-        }
-        cout << "> " << msg << endl;
-        process_get_request(newSd, msg, bytesRead);
-        close(newSd);
-        cout << "Closed connection" << endl;
+        threadList.push_back(thread(worker, newSd));
     }
-    close(serverSd);
+
+    for (auto &t : threadList) 
+    {
+        if (t.joinable())
+        {
+            t.join();
+        }
+    }
+   
     return 0;   
 }
